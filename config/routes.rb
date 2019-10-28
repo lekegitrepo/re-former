d25de89f1c4f5e92f6a99800553993ef59cf0720
Adeleke Temitayo <lekedevrepo@gmail.com>
2019-10-28 05:43:46 -0600

    use resouceful route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/show'
   resources :users, except: [:destroy]
 end
